fe597cdad47cb7711ccba985861c9e8eeab3557f
Andrei Kiss <ubervoltage@gmail.com>
2018-02-20 15:54:14 +0000

    derived TimeSplit from Calculator

--- src/calc/TimeSplit.cpp
+++ src/calc/TimeSplit.cpp
@@ -1,5 +1,6 @@
 #include "TimeSplit.hpp"
 #include <cmath>
+#include <sstream>
 
 TimeSplit::TimeSplit()
 {
@@ -18,14 +19,28 @@ TimeSplit::TimeSplit(int new_min, double new_sec)
     set_inputTime(new_min, new_sec);
 }
 
+TimeSplit::TimeSplit(const std::vector<std::string>& in_arg_list)
+{
+    setargs(in_arg_list);
+}
+
+void TimeSplit::setargs(const std::vector<std::string>& in_arg_list)
+{
+    if(in_arg_list.size() < 2 || in_arg_list.size() > 3)
+        throw std::invalid_argument("invalid number of arguments, must be exactly 2 arguments");
+    
+    m_input_time.first  = atof(in_arg_list[0].c_str());
+    m_input_time.second = atof(in_arg_list[1].c_str());
+}
+
 void TimeSplit::clearargs()
 {
-    input_time = std::pair<int, double> {0, 0};
+    set_inputTime(0, 0);
 }
 
 void TimeSplit::clearresult()
 {
-    result_time = std::pair<int, double> {0, 0};
+    m_result = 0;
 }
 
 void TimeSplit::clearall()
@@ -34,47 +49,41 @@ void TimeSplit::clearall()
     clearresult();
 }
 
-void TimeSplit::calculate()
+double TimeSplit::calculate()
 {
-    if (input_time.second >= 60)
+    if (m_input_time.second >= 60)
         throw std::domain_error("invalid number of seconds");
     
-    if (input_time.first != 0)
+    if (m_input_time.first != 0)
     {
-        double wseconds = ((input_time.first*60) + input_time.second) / 120;
+        m_result = ((m_input_time.first*60) + m_input_time.second) / 2;
+        /*
         double minutes = 0;
         double seconds = modf(wseconds, &minutes) * 60;
         result_time.first = minutes;
         result_time.second = seconds;
+        */
     }
     else
     {
-        double seconds = input_time.second / 2;
+        m_result = m_input_time.second / 2;
+        /*
         result_time.first = 0;
         result_time.second = seconds;
+        */
     }
+    return m_result;
 }
 
 std::pair<int, double> TimeSplit::get_inputTime()
 {
-    return input_time;
+    return m_input_time;
 }
 
 void TimeSplit::get_inputTime(int& minutes, double& seconds)
 {
-    minutes = input_time.first;
-    seconds = input_time.second;
-}
-
-std::pair<int, double> TimeSplit::getResult() const
-{
-    return result_time;
-}
-
-void TimeSplit::getResult(int& minutes, double& seconds)
-{
-    minutes = result_time.first;
-    seconds = result_time.second;
+    minutes = m_input_time.first;
+    seconds = m_input_time.second;
 }
 
 
@@ -85,7 +94,7 @@ void TimeSplit::set_inputTime(std::pair<int, double> new_input_time)
     if (new_input_time.second < 0 || new_input_time.second >= 60)
         throw std::domain_error("new input time value \"seconds\" cannot be lower than 0 seconds or higher than or equal to 60 seconds");
     
-    input_time = new_input_time;
+    m_input_time = new_input_time;
 }
 
 void TimeSplit::set_inputTime(int min, double sec)
@@ -95,25 +104,31 @@ void TimeSplit::set_inputTime(int min, double sec)
 }
 
 
-std::pair<int, double> TimeSplit::operator()(std::pair<int, double> in_time)
+double TimeSplit::operator()(std::pair<int, double> in_time)
 {
     clearall();
     set_inputTime(in_time);
-    calculate();
-    return getResult();
+    return calculate();
 }
 
-std::pair<int, double> TimeSplit::operator()(int min, double sec)
+double TimeSplit::operator()(int min, double sec)
 {
-    clearall();
-    set_inputTime(min, sec);
-    calculate();
-    return getResult();
+    return (*this)(std::pair<int, double> {min, sec});
 }
 
-std::ostream& operator<< (std::ostream& os, const TimeSplit& ts)
+std::string seconds_to_minsAndSecs(double secs)
 {
-    std::pair<int, double> out_val = ts.getResult();
-    os << out_val.first << "m" << out_val.second << "s";
-    return os;
+    std::stringstream ss;
+    if (secs < 60)
+    {
+        ss << "0min" << secs << "sec";
+    }
+    else
+    {
+        double minutes = 0;
+        double seconds = modf(secs/60, &minutes) * 60;
+        ss << minutes << "min" << seconds << "sec";
+    }
+    
+    return ss.str();
 }
--- src/calc/TimeSplit.hpp
+++ src/calc/TimeSplit.hpp
@@ -4,38 +4,39 @@
 #include <utility>
 #include <stdexcept>
 #include <ostream>
+#include <string>
+#include "Calculator.hpp"
 
 // inherit from Calculator and store result in m_result as seconds, usage of getResult() will return
 // just the number of seconds
 // provide a function to convert the number of seconds (double) into a pair of values min and sec (string), non-class
 
-class TimeSplit
+class TimeSplit : public Calculator
 {
 private:
-    std::pair<int, double> input_time;
-    std::pair<int, double> result_time;
+    std::pair<int, double> m_input_time;
 public:
     TimeSplit();
     TimeSplit(std::pair<int, double>);
     TimeSplit(int, double);
+    TimeSplit(const std::vector<std::string>& in_arg_list);
     
-    void clearargs();
-    void clearresult();
-    void clearall();
-    void calculate();
+    virtual void setargs(const std::vector<std::string>& in_arg_list);
+    virtual void clearargs();
+    virtual void clearresult();
+    virtual void clearall();
+    virtual double calculate();
     
     std::pair<int, double> get_inputTime();
     void get_inputTime(int& minutes, double& seconds);
-    std::pair<int, double> getResult() const;
-    void getResult(int& minutes, double& seconds);
     
     void set_inputTime(std::pair<int, double>);
     void set_inputTime(int, double);
     
-    std::pair<int, double> operator()(std::pair<int, double>);
-    std::pair<int, double> operator()(int, double);
+    double operator()(std::pair<int, double>);
+    double operator()(int, double);
 };
 
-std::ostream& operator<< (std::ostream& os, const TimeSplit& ts);
+std::string seconds_to_minsAndSecs(double secs);
 
 #endif
